25938e63753f6b9e7dde75ff2e2ccd6103bf3ed2
Koalaphant <andrew.wardjones@icloud.com>
2024-11-20 01:33:30 +0000

    moved questions to its own file

---
+++ questions.js
@@ -0,0 +1,74 @@
+export const questions = [
+  {
+    question: "What is the capital of Australia?",
+    answers: {
+      1: "Canberra",
+      2: "Sydney",
+      3: "Perth",
+      4: "Australasia",
+    },
+    correctAnswer: 1,
+    imageLink:
+      "https://images.pexels.com/photos/68704/pexels-photo-68704.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  {
+    question: "Which planet is known as the Red Planet?",
+    answers: {
+      1: "Mars",
+      2: "Venus",
+      3: "Jupiter",
+      4: "Saturn",
+    },
+    correctAnswer: 1,
+    imageLink:
+      "https://images.pexels.com/photos/73873/star-clusters-rosette-nebula-star-galaxies-73873.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  {
+    question: "Who wrote 'Romeo and Juliet'?",
+    answers: {
+      1: "William Shakespeare",
+      2: "Charles Dickens",
+      3: "Jane Austen",
+      4: "Mark Twain",
+    },
+    correctAnswer: 1,
+    imageLink:
+      "https://images.pexels.com/photos/46274/pexels-photo-46274.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  {
+    question: "Which ocean is the largest?",
+    answers: {
+      1: "Atlantic Ocean",
+      2: "Indian Ocean",
+      3: "Arctic Ocean",
+      4: "Pacific Ocean",
+    },
+    correctAnswer: 4,
+    imageLink:
+      "https://images.pexels.com/photos/6585322/pexels-photo-6585322.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  {
+    question: "What is the tallest mountain in the world?",
+    answers: {
+      1: "Mount Everest",
+      2: "K2",
+      3: "Kangchenjunga",
+      4: "Mount Kilimanjaro",
+    },
+    correctAnswer: 1,
+    imageLink:
+      "https://images.pexels.com/photos/9754/mountains-clouds-forest-fog.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  {
+    question: "Who painted the Mona Lisa?",
+    answers: {
+      1: "Leonardo da Vinci",
+      2: "Vincent van Gogh",
+      3: "Pablo Picasso",
+      4: "Claude Monet",
+    },
+    correctAnswer: 1,
+    imageLink:
+      "https://images.pexels.com/photos/3004109/pexels-photo-3004109.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+];
